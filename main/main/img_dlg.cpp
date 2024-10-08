4d51cc4ccb7d2f7182208948a2a72371265eaa77
Bumsik Kim <bumsikki@usc.edu>
2024-10-07 22:15:21 -0500

    init commit

---
+++ main/main/img_dlg.cpp
@@ -0,0 +1,38 @@
+﻿// img_dlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "main.h"
+#include "afxdialogex.h"
+#include "img_dlg.h"
+
+#include <iostream>
+#include "mainDlg.h"
+
+// img_dlg 대화 상자
+
+IMPLEMENT_DYNAMIC(img_dlg, CDialogEx)
+
+img_dlg::img_dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_img_dlg, pParent)
+{
+	parent = pParent;
+	/*CmainDlg* mainDlg = (CmainDlg*)parent;*/
+
+}
+
+img_dlg::~img_dlg()
+{
+}
+
+void img_dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(img_dlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// img_dlg 메시지 처리기
---
+++ main/main/img_dlg.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// img_dlg 대화 상자
+
+class img_dlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(img_dlg)
+
+public:
+	img_dlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~img_dlg();
+
+	CWnd* parent;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_img_dlg };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
